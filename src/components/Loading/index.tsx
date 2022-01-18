5e00f12f7f5701eccd08458a10869ee8c9514a8d
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 14:47:03 -0300

    feat: create component Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+
+import { Container } from "./styles";
+
+interface ILoadingProps {
+  show: boolean;
+}
+
+const Loading: NextPage<ILoadingProps> = ({ show }) => {
+  return (
+    <Container show={show}>
+      <p>Carregando...</p>
+    </Container>
+  );
+};
+
+export default Loading;
