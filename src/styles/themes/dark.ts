2b9fd76289091e58f8b8e3d0eba9722ac0229fb2
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 13:11:23 -0300

    feat: create dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333333",
+    secondary: "#c62e65",
+
+    background: "#222222",
+    text: "#ffffff",
+  },
+};
