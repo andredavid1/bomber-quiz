8ca4fc4946b494725491c2a8a9747dd850359ee1
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 13:32:45 -0300

    feat: create config context, implementation theme provider and create toggle themes

---
+++ src/contexts/ConfigContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { DefaultTheme, ThemeProvider } from "styled-components";
+import dark from "styles/themes/dark";
+import light from "styles/themes/light";
+/* import { AuthProvider } from "./AuthContext"; */
+
+interface IConfigContextProps {
+  loading: boolean;
+  theme: DefaultTheme;
+  toggleLoading: (show: boolean) => void;
+  toggleTheme: () => void;
+}
+
+const ConfigContext = createContext({
+  loading: false,
+} as IConfigContextProps);
+
+interface IConfigProviderProps {
+  children: ReactNode;
+}
+
+export const ConfigProvider = ({ children }: IConfigProviderProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [theme, setTheme] = useState<DefaultTheme>(light);
+
+  useEffect(() => {
+    const themeSelected = localStorage.getItem("bomberQuizTheme");
+    setTheme(themeSelected ? JSON.parse(themeSelected) : light);
+  }, []);
+
+  const toggleLoading = (show: boolean) => {
+    setLoading(show);
+  };
+
+  const toggleTheme = () => {
+    if (theme.title === "light") {
+      setTheme(dark);
+      localStorage.setItem("bomberQuizTheme", JSON.stringify(dark));
+    } else {
+      setTheme(light);
+      localStorage.setItem("bomberQuizTheme", JSON.stringify(light));
+    }
+  };
+
+  return (
+    <ConfigContext.Provider
+      value={{ loading, theme, toggleLoading, toggleTheme }}
+    >
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ConfigContext.Provider>
+  );
+};
+
+export default ConfigContext;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { ConfigProvider } from "contexts/ConfigContext";
+import GlobalStyles from "styles/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
 
 export default MyApp;
