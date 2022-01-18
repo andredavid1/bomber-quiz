1643a389b75d506630e4a581192e1e9eafd202c4
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 14:15:48 -0300

    feat: create API logout

---
+++ src/pages/auth/logout.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { removeCookies } from "cookies-next";
+import AppError from "erros/AppError";
+
+interface IResponse {
+  success: boolean;
+  error?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponse>
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        removeCookies("tokenBomberQuiz", {
+          req,
+          res,
+          domain: process.env.DOMAIN,
+          path: "/",
+          maxAge: 60 * 60, //1 hour
+          sameSite: "lax",
+        });
+
+        return res.status(200).json({ success: true });
+      } catch (err: any) {
+        if (err instanceof AppError) {
+          return res
+            .status(err.statusCode)
+            .json({ success: false, error: err.message });
+        } else {
+          return res
+            .status(500)
+            .json({ success: false, error: "Erro na conexão com o servidor." });
+        }
+      }
+
+    default:
+      return res
+        .status(405)
+        .json({ success: false, error: "Método não suportado." });
+  }
+}
