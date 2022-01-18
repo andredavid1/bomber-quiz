70b7973d3b80f5008543e3321ae00cf7b3552308
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 14:18:44 -0300

    feat: create API verify

---
+++ src/pages/auth/verify.ts
@@ -0,0 +1,78 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { JwtPayload, verify } from "jsonwebtoken";
+import AppError from "erros/AppError";
+
+interface IUserLogged {
+  id: string;
+  name: string;
+  level: string;
+}
+
+interface IResponse {
+  success: boolean;
+  payload?: IUserLogged;
+  error?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponse>
+) {
+  const { method } = req;
+  const { token } = req.body;
+  const jwtSecret = process.env.JWT_SECRET;
+
+  switch (method) {
+    case "POST":
+      try {
+        let tokenBomberQuiz = token.split("tokenBomberQuiz=")[1];
+
+        if (!tokenBomberQuiz) {
+          tokenBomberQuiz = token;
+        }
+
+        if (!tokenBomberQuiz) {
+          throw new AppError("Acesso não autorizado.", 401);
+        }
+
+        if (!jwtSecret) {
+          throw new AppError(
+            "Variável de ambiente JWT_SECRET não encontrada.",
+            400
+          );
+        }
+
+        const payload: JwtPayload = verify(
+          tokenBomberQuiz,
+          jwtSecret
+        ) as JwtPayload;
+
+        if (!payload) {
+          throw new AppError("Acesso não autorizado.", 401);
+        }
+
+        const userLogged = {
+          id: payload.id,
+          name: payload.name,
+          level: payload.level,
+        };
+
+        return res.status(200).json({ success: true, payload: userLogged });
+      } catch (err: any) {
+        if (err instanceof AppError) {
+          return res
+            .status(err.statusCode)
+            .json({ success: false, error: err.message });
+        } else {
+          return res
+            .status(500)
+            .json({ success: false, error: "Erro na conexão com o servidor." });
+        }
+      }
+
+    default:
+      return res
+        .status(405)
+        .json({ success: false, error: "Método não suportado." });
+  }
+}
