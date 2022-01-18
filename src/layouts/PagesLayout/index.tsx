9730da76fcf21666f2490f94d00a4392b40022ec
andredavid1 <andredavid1@yahoo.com.br>
2022-01-18 14:47:53 -0300

    feat: create Layout Page

---
+++ src/layouts/PagesLayout/index.tsx
@@ -0,0 +1,37 @@
+import { NextPage } from "next";
+import { ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+import Loading from "components/Loading";
+import useConfig from "hooks/useConfig";
+
+import { Container } from "./styles";
+import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
+
+interface ILayoutCustomerProps {
+  children: ReactNode;
+}
+
+const LayoutCustomer: NextPage<ILayoutCustomerProps> = ({ children }) => {
+  const { loading } = useConfig();
+
+  return (
+    <Container>
+      <Head>
+        <title>BomberQuiz</title>
+        <meta
+          name="description"
+          content="Quiz de matérias de bombeiros de Goiás"
+        />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+      <ToastContainer position="top-right" />
+      <Loading show={loading} />
+    </Container>
+  );
+};
+
+export default LayoutCustomer;
