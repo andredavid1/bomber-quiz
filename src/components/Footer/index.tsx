050a3cdf542d38e70c0da35b3ed62fa14fb17208
andredavid1 <andredavid1@yahoo.com.br>
2022-01-19 00:31:11 -0300

    feat: create component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Container } from "./styles";
+
+const Footer = () => {
+  return (
+    <Container>
+      <span>A&T Desenvolvimento Web</span>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  background: ${(props) => props.theme.colors.primary};
+  padding: 8px 0;
+  text-align: center;
+  width: 100%;
+`;
